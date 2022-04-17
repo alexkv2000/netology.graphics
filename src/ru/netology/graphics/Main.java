497e8eff0280f6752ad04ed37fb54eb243040c2f
alexkv2000 <alexkv2000@mail.ru>
2022-04-17 16:33:49 +0300

    disconnect export to file

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -11,21 +11,22 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Convert converter = new Convert(600, 1200, 0, new SchemaImage()); // Создайте тут объект вашего класса конвертера
-        GServer server = new GServer(converter); // Создаём объект сервера
-        server.start(); // Запускаем*/
-        // Или то же, но с сохранением в файл:
+        Convert converter = new Convert(600, 1200, 0, new SchemaImage()); // Г‘Г®Г§Г¤Г Г©ГІГҐ ГІГіГІ Г®ГЎГєГҐГЄГІ ГўГ ГёГҐГЈГ® ГЄГ«Г Г±Г±Г  ГЄГ®Г­ГўГҐГ°ГІГҐГ°Г 
+        GServer server = new GServer(converter); // Г‘Г®Г§Г¤Г ВёГ¬ Г®ГЎГєГҐГЄГІ Г±ГҐГ°ГўГҐГ°Г 
+        server.start(); // Г‡Г ГЇГіГ±ГЄГ ГҐГ¬*/
+        // Г€Г«ГЁ ГІГ® Г¦ГҐ, Г­Г® Г± Г±Г®ГµГ°Г Г­ГҐГ­ГЁГҐГ¬ Гў ГґГ Г©Г«:
+        /*
         PrintWriter fileWriter = new PrintWriter(new File("converted-image.txt"));
         converter.setMaxWidth(300);
         converter.setMaxHeight(600);
         converter.setMaxRatio(0);
-        converter.setTextColorSchema(". , : \" ! ? % № $ # @");
+        converter.setTextColorSchema(". , : \" ! ? % В№ $ # @");
         //  https://invigor.by/assets/images/resources/191/3877-o.jpg
         try {
             fileWriter.write(converter.convert("https://img.favcars.com/audi/r8/pictures_audi_r8_2007_8_1024x768.jpg"));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        fileWriter.close();
+        fileWriter.close();*/
     }
 }
